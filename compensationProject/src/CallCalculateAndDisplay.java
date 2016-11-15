a860f7366a17eb2d842a881df67f4d7fd7602bed
กนกวรรณ\Kanokwan <pang4965@gmail.com>
2016-11-15 17:15:33 +0700

    Start project compensation

---
+++ compensationProject/src/CallCalculateAndDisplay.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CallCalculateAndDisplay {
+
+	public static void main(String[] args) {
+		String name;
+		double salary;
+		double turnover;
+		Scanner scan= new Scanner(System.in);
+		System.out.print("Enter name :");
+		name = scan.nextLine();
+		System.out.print("Enter salary :");
+		salary = scan.nextInt();
+		System.out.print("Enter turnover :");
+		turnover = scan.nextInt();
+	}
+
+}
